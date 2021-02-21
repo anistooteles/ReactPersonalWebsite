b515ab6b597eff2c2a34012e354e224a614e45d8
Anahi Ruiz <anahiruiz@Anahis-MacBook-Pro.local>
2021-02-20 18:18:01 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import { useState } from 'react';
+import Navigation from './components/Navigation';
+
+const App = () => {
+  const [state, setstate] = useState({ lang: 'eng', theme: 'light' });
+
+  const changeLang = () => {
+    console.log('change Language!');
+    setstate({ ...state, lang: state.lang === 'eng' ? 'esp' : 'eng' });
+  };
+
+  const changeLight = () => {
+    console.log('change Light!');
+    setstate({ ...state, theme: state.theme === 'light' ? 'dark' : 'light' });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation action={changeLight}></Navigation>
+      {JSON.stringify(state)}
     </div>
   );
-}
+};
 
 export default App;
